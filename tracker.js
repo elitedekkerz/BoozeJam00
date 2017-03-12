c9670fa565afe7a7220ceee0d67617a914dc8422
Max Sandholm <max@sandholm.org>
2017-03-12 03:22:17 +0200

    nyt toimii

--- tracker.js
+++ tracker.js
@@ -1,13 +1,5 @@
 function playMusic(){
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', './oispaes.mod');
-  xhr.responseType = 'arraybuffer';
-  xhr.onload = function(e){
-    if (this.status == 200){
-      player = window.neoart.FileLoader.load(this.response);
-      player.loopSong = 1
-      player.play();
-    }
-  };
-  xhr.send();
+  var player = new Modplayer();
+  player.load("oispaes.mod");
+  player.play();
 };
